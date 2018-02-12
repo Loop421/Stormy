6b0479283d33d71155954b673f6a709566fdf109
Loop421 <lopez.e421@gmail.com>
2018-02-11 22:40:51 -0500

    simple weather app.

---
+++ app/src/main/java/com/mongo/stormy/weather/Day.java
@@ -0,0 +1,70 @@
+package com.mongo.stormy.weather;
+
+
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.TimeZone;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.util.Date;
+
+public class Day {
+    private long mTime;
+    private String mSummary;
+    private double mTemperatureMax;
+    private String mIcon;
+    private String mTimeZone;
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    public int getTemperatureMax() {
+        return (int)Math.round(mTemperatureMax);
+    }
+
+    public void setTemperatureMax(double temperatureMax) {
+        mTemperatureMax = temperatureMax;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+
+    public int getIconId(){
+        return Forecast.getIconId(mIcon);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public String getDayOfTheWeek() {
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE");
+        formatter.setTimeZone(TimeZone.getTimeZone(mTimeZone));
+        Date dateTime = new Date(mTime * 1000);
+
+        return formatter.format(dateTime);
+    }
+}
